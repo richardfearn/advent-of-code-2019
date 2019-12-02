fe28dc9bb62c7f6d637068f4be3d99cfd813f04f
Richard Fearn <richardfearn@gmail.com>
2019-12-02 09:51:41 +0000

    Day 1: add tests

--- day1/part1.py
+++ day1/part1.py
@@ -2,14 +2,20 @@
 
 import sys
 
+
 def fuel_required(mass):
     return (mass // 3) - 2
 
-masses = sys.stdin.readlines()
-masses = list(map(int, masses))
 
-fuel = list(map(fuel_required, masses))
-for f in fuel:
-    print(f)
+def main():
 
-print("Total: %d" % sum(fuel))
+    masses = sys.stdin.readlines()
+    masses = list(map(int, masses))
+
+    fuel = list(map(fuel_required, masses))
+
+    print(sum(fuel))
+
+
+if __name__ == "__main__":
+    main()
