dcd475e46b611bf3f5abe379153ac9a39b962f55
Richard Fearn <richardfearn@gmail.com>
2019-12-03 13:31:25 +0000

    Add tests using input for days 1-3

--- day1/tests.py
+++ day1/tests.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import unittest
+import utils
 from day1 import part1, part2
 
 
@@ -18,6 +19,12 @@ class Part1Tests(unittest.TestCase):
     def test_example_4(self):
         self.assertEqual(part1.fuel_required(100756), 33583)
 
+    def test_with_input(self):
+        lines = utils.read_input()
+        masses = map(int, lines)
+        fuel = [part1.fuel_required(mass) for mass in masses]
+        self.assertEqual(sum(fuel), 3335787)
+
 
 class Part2Tests(unittest.TestCase):
 
@@ -29,6 +36,12 @@ class Part2Tests(unittest.TestCase):
 
     def test_example_3(self):
         self.assertEqual(part2.fuel_required(100756), 50346)
+
+    def test_with_input(self):
+        lines = utils.read_input()
+        masses = map(int, lines)
+        fuel = [part2.fuel_required(mass) for mass in masses]
+        self.assertEqual(sum(fuel), 5000812)
 
 
 if __name__ == '__main__':
