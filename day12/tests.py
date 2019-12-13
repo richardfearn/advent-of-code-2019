8309bebb9438521d39ac98dbce1a4ab7362ea43a
Richard Fearn <richardfearn@gmail.com>
2019-12-13 01:51:46 +0000

    Day 12

---
+++ day12/tests.py
@@ -0,0 +1,244 @@
+#! /usr/bin/python3
+
+import unittest
+from day12.n_body_problem import Vector, Moon, System
+import utils
+
+
+PART_1_EXAMPLE_1_MOONS_INITIAL = """
+<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>
+"""
+
+PART_1_EXAMPLE_1_EXPECTED_TEXT = """
+After 0 steps:
+pos=<x=-1, y=  0, z= 2>, vel=<x= 0, y= 0, z= 0>
+pos=<x= 2, y=-10, z=-7>, vel=<x= 0, y= 0, z= 0>
+pos=<x= 4, y= -8, z= 8>, vel=<x= 0, y= 0, z= 0>
+pos=<x= 3, y=  5, z=-1>, vel=<x= 0, y= 0, z= 0>
+
+After 1 step:
+pos=<x= 2, y=-1, z= 1>, vel=<x= 3, y=-1, z=-1>
+pos=<x= 3, y=-7, z=-4>, vel=<x= 1, y= 3, z= 3>
+pos=<x= 1, y=-7, z= 5>, vel=<x=-3, y= 1, z=-3>
+pos=<x= 2, y= 2, z= 0>, vel=<x=-1, y=-3, z= 1>
+
+After 2 steps:
+pos=<x= 5, y=-3, z=-1>, vel=<x= 3, y=-2, z=-2>
+pos=<x= 1, y=-2, z= 2>, vel=<x=-2, y= 5, z= 6>
+pos=<x= 1, y=-4, z=-1>, vel=<x= 0, y= 3, z=-6>
+pos=<x= 1, y=-4, z= 2>, vel=<x=-1, y=-6, z= 2>
+
+After 3 steps:
+pos=<x= 5, y=-6, z=-1>, vel=<x= 0, y=-3, z= 0>
+pos=<x= 0, y= 0, z= 6>, vel=<x=-1, y= 2, z= 4>
+pos=<x= 2, y= 1, z=-5>, vel=<x= 1, y= 5, z=-4>
+pos=<x= 1, y=-8, z= 2>, vel=<x= 0, y=-4, z= 0>
+
+After 4 steps:
+pos=<x= 2, y=-8, z= 0>, vel=<x=-3, y=-2, z= 1>
+pos=<x= 2, y= 1, z= 7>, vel=<x= 2, y= 1, z= 1>
+pos=<x= 2, y= 3, z=-6>, vel=<x= 0, y= 2, z=-1>
+pos=<x= 2, y=-9, z= 1>, vel=<x= 1, y=-1, z=-1>
+
+After 5 steps:
+pos=<x=-1, y=-9, z= 2>, vel=<x=-3, y=-1, z= 2>
+pos=<x= 4, y= 1, z= 5>, vel=<x= 2, y= 0, z=-2>
+pos=<x= 2, y= 2, z=-4>, vel=<x= 0, y=-1, z= 2>
+pos=<x= 3, y=-7, z=-1>, vel=<x= 1, y= 2, z=-2>
+
+After 6 steps:
+pos=<x=-1, y=-7, z= 3>, vel=<x= 0, y= 2, z= 1>
+pos=<x= 3, y= 0, z= 0>, vel=<x=-1, y=-1, z=-5>
+pos=<x= 3, y=-2, z= 1>, vel=<x= 1, y=-4, z= 5>
+pos=<x= 3, y=-4, z=-2>, vel=<x= 0, y= 3, z=-1>
+
+After 7 steps:
+pos=<x= 2, y=-2, z= 1>, vel=<x= 3, y= 5, z=-2>
+pos=<x= 1, y=-4, z=-4>, vel=<x=-2, y=-4, z=-4>
+pos=<x= 3, y=-7, z= 5>, vel=<x= 0, y=-5, z= 4>
+pos=<x= 2, y= 0, z= 0>, vel=<x=-1, y= 4, z= 2>
+
+After 8 steps:
+pos=<x= 5, y= 2, z=-2>, vel=<x= 3, y= 4, z=-3>
+pos=<x= 2, y=-7, z=-5>, vel=<x= 1, y=-3, z=-1>
+pos=<x= 0, y=-9, z= 6>, vel=<x=-3, y=-2, z= 1>
+pos=<x= 1, y= 1, z= 3>, vel=<x=-1, y= 1, z= 3>
+
+After 9 steps:
+pos=<x= 5, y= 3, z=-4>, vel=<x= 0, y= 1, z=-2>
+pos=<x= 2, y=-9, z=-3>, vel=<x= 0, y=-2, z= 2>
+pos=<x= 0, y=-8, z= 4>, vel=<x= 0, y= 1, z=-2>
+pos=<x= 1, y= 1, z= 5>, vel=<x= 0, y= 0, z= 2>
+
+After 10 steps:
+pos=<x= 2, y= 1, z=-3>, vel=<x=-3, y=-2, z= 1>
+pos=<x= 1, y=-8, z= 0>, vel=<x=-1, y= 1, z= 3>
+pos=<x= 3, y=-6, z= 1>, vel=<x= 3, y= 2, z=-3>
+pos=<x= 2, y= 0, z= 4>, vel=<x= 1, y=-1, z=-1>
+"""
+
+PART_1_EXAMPLE_2_MOONS_INITIAL = """
+<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>
+"""
+
+PART_1_EXAMPLE_2_EXPECTED_TEXT = """
+After 0 steps:
+pos=<x= -8, y=-10, z=  0>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  5, y=  5, z= 10>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  2, y= -7, z=  3>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  9, y= -8, z= -3>, vel=<x=  0, y=  0, z=  0>
+
+After 10 steps:
+pos=<x= -9, y=-10, z=  1>, vel=<x= -2, y= -2, z= -1>
+pos=<x=  4, y= 10, z=  9>, vel=<x= -3, y=  7, z= -2>
+pos=<x=  8, y=-10, z= -3>, vel=<x=  5, y= -1, z= -2>
+pos=<x=  5, y=-10, z=  3>, vel=<x=  0, y= -4, z=  5>
+
+After 20 steps:
+pos=<x=-10, y=  3, z= -4>, vel=<x= -5, y=  2, z=  0>
+pos=<x=  5, y=-25, z=  6>, vel=<x=  1, y=  1, z= -4>
+pos=<x= 13, y=  1, z=  1>, vel=<x=  5, y= -2, z=  2>
+pos=<x=  0, y=  1, z=  7>, vel=<x= -1, y= -1, z=  2>
+
+After 30 steps:
+pos=<x= 15, y= -6, z= -9>, vel=<x= -5, y=  4, z=  0>
+pos=<x= -4, y=-11, z=  3>, vel=<x= -3, y=-10, z=  0>
+pos=<x=  0, y= -1, z= 11>, vel=<x=  7, y=  4, z=  3>
+pos=<x= -3, y= -2, z=  5>, vel=<x=  1, y=  2, z= -3>
+
+After 40 steps:
+pos=<x= 14, y=-12, z= -4>, vel=<x= 11, y=  3, z=  0>
+pos=<x= -1, y= 18, z=  8>, vel=<x= -5, y=  2, z=  3>
+pos=<x= -5, y=-14, z=  8>, vel=<x=  1, y= -2, z=  0>
+pos=<x=  0, y=-12, z= -2>, vel=<x= -7, y= -3, z= -3>
+
+After 50 steps:
+pos=<x=-23, y=  4, z=  1>, vel=<x= -7, y= -1, z=  2>
+pos=<x= 20, y=-31, z= 13>, vel=<x=  5, y=  3, z=  4>
+pos=<x= -4, y=  6, z=  1>, vel=<x= -1, y=  1, z= -3>
+pos=<x= 15, y=  1, z= -5>, vel=<x=  3, y= -3, z= -3>
+
+After 60 steps:
+pos=<x= 36, y=-10, z=  6>, vel=<x=  5, y=  0, z=  3>
+pos=<x=-18, y= 10, z=  9>, vel=<x= -3, y= -7, z=  5>
+pos=<x=  8, y=-12, z= -3>, vel=<x= -2, y=  1, z= -7>
+pos=<x=-18, y= -8, z= -2>, vel=<x=  0, y=  6, z= -1>
+
+After 70 steps:
+pos=<x=-33, y= -6, z=  5>, vel=<x= -5, y= -4, z=  7>
+pos=<x= 13, y= -9, z=  2>, vel=<x= -2, y= 11, z=  3>
+pos=<x= 11, y= -8, z=  2>, vel=<x=  8, y= -6, z= -7>
+pos=<x= 17, y=  3, z=  1>, vel=<x= -1, y= -1, z= -3>
+
+After 80 steps:
+pos=<x= 30, y= -8, z=  3>, vel=<x=  3, y=  3, z=  0>
+pos=<x= -2, y= -4, z=  0>, vel=<x=  4, y=-13, z=  2>
+pos=<x=-18, y= -7, z= 15>, vel=<x= -8, y=  2, z= -2>
+pos=<x= -2, y= -1, z= -8>, vel=<x=  1, y=  8, z=  0>
+
+After 90 steps:
+pos=<x=-25, y= -1, z=  4>, vel=<x=  1, y= -3, z=  4>
+pos=<x=  2, y= -9, z=  0>, vel=<x= -3, y= 13, z= -1>
+pos=<x= 32, y= -8, z= 14>, vel=<x=  5, y= -4, z=  6>
+pos=<x= -1, y= -2, z= -8>, vel=<x= -3, y= -6, z= -9>
+
+After 100 steps:
+pos=<x=  8, y=-12, z= -9>, vel=<x= -7, y=  3, z=  0>
+pos=<x= 13, y= 16, z= -3>, vel=<x=  3, y=-11, z= -5>
+pos=<x=-29, y=-11, z= -1>, vel=<x= -3, y=  7, z=  4>
+pos=<x= 16, y=-13, z= 23>, vel=<x=  7, y=  1, z=  1>
+"""
+
+
+class Part1Tests(unittest.TestCase):
+
+    def test_calc_gravity(self):
+        ganymede = Vector(3, 0, 0)
+        callisto = Vector(5, 0, 0)
+        self.assertEqual(Vector(1, 0, 0), System.calc_gravity(ganymede, callisto))
+        self.assertEqual(Vector(-1, 0, 0), System.calc_gravity(callisto, ganymede))
+
+    def test_example_1(self):
+        self.run_example(PART_1_EXAMPLE_1_MOONS_INITIAL, PART_1_EXAMPLE_1_EXPECTED_TEXT, 179)
+
+    def test_example_2(self):
+        self.run_example(PART_1_EXAMPLE_2_MOONS_INITIAL, PART_1_EXAMPLE_2_EXPECTED_TEXT, 1940)
+
+    def run_example(self, moons_initial_text, expected_text, expected_final_energy):
+
+        system = parse_text(moons_initial_text)
+        expected = parse_expected_text(expected_text)
+
+        steps_to_check = list(expected.keys())
+
+        steps_run_so_far = 0
+        for step in steps_to_check:
+            while steps_run_so_far < step:
+                system = system.run()
+                steps_run_so_far += 1
+            self.assertEqual(expected[steps_run_so_far], system, "after step %d" % step)
+
+        self.assertEqual(expected_final_energy, system.total_energy())
+
+    def test_with_input(self):
+        system = read_input()
+        for i in range(1000):
+            system = system.run()
+        self.assertEqual(12466, system.total_energy())
+
+
+class Part2Tests(unittest.TestCase):
+
+    def test_example_1(self):
+        initial_system = parse_text(PART_1_EXAMPLE_1_MOONS_INITIAL)
+
+        # slow way
+        system = initial_system
+        for i in range(2772):
+            system = system.run()
+        self.assertEqual(system, initial_system)
+
+        # fast way
+        self.assertEqual(2772, initial_system.steps_to_return_to_same_state())
+
+    def test_example_2(self):
+        system = parse_text(PART_1_EXAMPLE_2_MOONS_INITIAL)
+        self.assertEqual(4686774924, system.steps_to_return_to_same_state())
+
+    def test_with_input(self):
+        system = read_input()
+        self.assertEqual(360689156787864, system.steps_to_return_to_same_state())
+
+
+def parse_expected_text(text):
+    lines = text.strip().split("\n")
+    expected = chunks(lines, 6)
+    expected = [(int(group[0].split(" ")[1]), parse_lines(group[1:5])) for group in expected]
+    expected = dict(expected)
+    return expected
+
+
+def chunks(lst, n):
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
+
+def read_input():
+    return parse_lines(utils.read_input())
+
+
+def parse_text(text):
+    return parse_lines(text.strip().split("\n"))
+
+
+def parse_lines(lines):
+    return System([Moon.from_line(line) for line in lines])
+
+
+if __name__ == '__main__':
+    unittest.main()
