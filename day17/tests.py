0bd36967aa66fc62559644eaa7cde8f8ea0b406f
Richard Fearn <richardfearn@gmail.com>
2019-12-17 22:17:30 +0000

    Day 17 - part 2 - partial solution
    Determines the path to visit every part of the scaffold, but doesn't yet split
    it into smaller (A/B/C) parts.

--- day17/tests.py
+++ day17/tests.py
@@ -14,6 +14,29 @@ PART_1_EXAMPLE_1 = """
 ..#####...^..
 """
 
+PART_2_EXAMPLE_1 = """
+#######...#####
+#.....#...#...#
+#.....#...#...#
+......#...#...#
+......#...###.#
+......#.....#.#
+^########...#.#
+......#.#...#.#
+......#########
+........#...#..
+....#########..
+....#...#......
+....#...#......
+....#...#......
+....#####......
+"""
+
+PART_2_EXAMPLE_1_PATH = """R,8,R,8,R,4,R,4,R,8,L,6,L,2,R,4,R,4,R,8,R,8,R,8,L,6,L,2"""
+
+PART_2_INPUT_PATH = "L,12,L,12,L,6,L,6,R,8,R,4,L,12,L,12,L,12,L,6,L,6,L,12,L,6,R,12,R,8,R,8,R,4,L,12,L,12,L,12,L,6," + \
+    "L,6,L,12,L,6,R,12,R,8,R,8,R,4,L,12,L,12,L,12,L,6,L,6,L,12,L,6,R,12,R,8"
+
 
 class Part1Tests(unittest.TestCase):
 
@@ -32,6 +55,23 @@ class Part1Tests(unittest.TestCase):
         intersections = view.find_intersections()
         alignment_parameters = [p[0] * p[1] for p in intersections]
         self.assertEqual(4800, sum(alignment_parameters))
+
+
+class Part2Tests(unittest.TestCase):
+
+    def test_example(self):
+        view = View(PART_2_EXAMPLE_1)
+        print(view.text)
+        view.end_pos = (0, 2)
+        self.assertEqual(PART_2_EXAMPLE_1_PATH, view.path_to_other_end())
+
+    def test_with_input(self):
+        program = Program(read_input())
+        cameras = Cameras(program)
+        view = cameras.get_view()
+        print(view.text)
+        view.end_pos = (32, 42)
+        self.assertEqual(PART_2_INPUT_PATH, view.path_to_other_end())
 
 
 def read_program(line):
