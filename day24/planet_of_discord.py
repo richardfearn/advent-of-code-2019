43180639f1ee59954c7577cb237abac032748203
Richard Fearn <richardfearn@gmail.com>
2019-12-30 01:31:52 +0000

    Day 24 - part 2

--- day24/planet_of_discord.py
+++ day24/planet_of_discord.py
@@ -1,5 +1,6 @@
 BUG = '#'
 EMPTY_SPACE = '.'
+UP_OR_DOWN = '?'
 
 
 class Grid:
@@ -67,6 +68,152 @@ class Grid:
                     tile_score = 1 << (y * 5 + x)
                     layout_rating += tile_score
         return layout_rating
+
+    def as_text(self):
+        return "\n".join(self.grid)
+
+
+EMPTY = """
+.....
+.....
+..?..
+.....
+.....
+"""
+
+
+class RecursiveGrid:
+
+    def __init__(self, grid, levels_up_and_down):
+        levels = [EMPTY] * levels_up_and_down + [grid] + [EMPTY] * levels_up_and_down
+        self.levels = [RecursiveGridLevel(level) for level in levels]
+        self.levels_up_and_down = levels_up_and_down
+        self.connect_levels()
+
+    def connect_levels(self):
+        for i in range(len(self.levels)):
+            if i > 0:
+                self.levels[i].up = self.levels[i - 1]
+            if i < (len(self.levels) - 1):
+                self.levels[i].down = self.levels[i + 1]
+
+    def step(self):
+        self.levels = [level.step() for level in self.levels]
+        self.connect_levels()
+
+    def total_bugs(self):
+        return sum(level.total_bugs() for level in self.levels)
+
+    def as_text(self):
+        texts = ["Depth %d:\n%s" % ((i - self.levels_up_and_down), self.levels[i].as_text())
+                 for i in range(len(self.levels))]
+        return "\n\n".join(texts)
+
+
+class RecursiveGridLevel:
+
+    def __init__(self, grid):
+        self.grid = grid.strip().split("\n")
+        self.up = None
+        self.down = None
+
+    def step(self):
+
+        new_grid = []
+
+        for y in range(5):
+
+            new_row = []
+
+            for x in range(5):
+
+                if (x == 2) and (y == 2):
+                    new_state = UP_OR_DOWN
+
+                else:
+
+                    old_state = self.grid[y][x]
+                    adjacent_bugs = 0
+
+                    # Left
+
+                    if x == 0:
+                        if self.up and self.up.grid[2][1] == BUG:
+                            adjacent_bugs += 1
+
+                    elif (x == 3) and (y == 2):
+                        if self.down:
+                            for dy in range(5):
+                                if self.down.grid[dy][4] == BUG:
+                                    adjacent_bugs += 1
+
+                    elif self.grid[y][x-1] == BUG:
+                        adjacent_bugs += 1
+
+                    # Right
+
+                    if x == 4:
+                        if self.up and self.up.grid[2][3] == BUG:
+                            adjacent_bugs += 1
+
+                    elif (x == 1) and (y == 2):
+                        if self.down:
+                            for dy in range(5):
+                                if self.down.grid[dy][0] == BUG:
+                                    adjacent_bugs += 1
+
+                    elif self.grid[y][x+1] == BUG:
+                        adjacent_bugs += 1
+
+                    # Up
+
+                    if y == 0:
+                        if self.up and self.up.grid[1][2] == BUG:
+                            adjacent_bugs += 1
+
+                    elif (x == 2) and (y == 3):
+                        if self.down:
+                            for dx in range(5):
+                                if self.down.grid[4][dx] == BUG:
+                                    adjacent_bugs += 1
+
+                    elif self.grid[y-1][x] == BUG:
+                        adjacent_bugs += 1
+
+                    # Down
+
+                    if y == 4:
+                        if self.up and self.up.grid[3][2] == BUG:
+                            adjacent_bugs += 1
+
+                    elif (x == 2) and (y == 1):
+                        if self.down:
+                            for dx in range(5):
+                                if self.down.grid[0][dx] == BUG:
+                                    adjacent_bugs += 1
+
+                    elif self.grid[y+1][x] == BUG:
+                        adjacent_bugs += 1
+
+                    # Determine new state
+
+                    if (old_state == BUG) and (adjacent_bugs != 1):
+                        new_state = EMPTY_SPACE
+
+                    elif (old_state == EMPTY_SPACE) and (adjacent_bugs in (1, 2)):
+                        new_state = BUG
+
+                    else:
+                        new_state = old_state
+
+                new_row.append(new_state)
+
+            new_grid.append("".join(new_row))
+
+        return RecursiveGridLevel("\n".join(new_grid))
+
+    def total_bugs(self):
+        return sum(row.count(BUG) for row in self.grid)
 
     def as_text(self):
         return "\n".join(self.grid)
--- day24/tests.py
+++ day24/tests.py
@@ -49,6 +49,93 @@ PART_1_EXAMPLE_REPEATED_STATE = """
 .#...
 """
 
+PART_2_EXAMPLE_INITIAL_STATE = """
+....#
+#..#.
+#.?##
+..#..
+#....
+"""
+
+PART_2_EXAMPLE_AFTER_10_MINS = """
+Depth -5:
+..#..
+.#.#.
+..?.#
+.#.#.
+..#..
+
+Depth -4:
+...#.
+...##
+..?..
+...##
+...#.
+
+Depth -3:
+#.#..
+.#...
+..?..
+.#...
+#.#..
+
+Depth -2:
+.#.##
+....#
+..?.#
+...##
+.###.
+
+Depth -1:
+#..##
+...##
+..?..
+...#.
+.####
+
+Depth 0:
+.#...
+.#.##
+.#?..
+.....
+.....
+
+Depth 1:
+.##..
+#..##
+..?.#
+##.##
+#####
+
+Depth 2:
+###..
+##.#.
+#.?..
+.#.##
+#.#..
+
+Depth 3:
+..###
+.....
+#.?..
+#....
+#...#
+
+Depth 4:
+.###.
+#..#.
+#.?..
+##.#.
+.....
+
+Depth 5:
+####.
+#..#.
+#.?#.
+####.
+.....
+"""
+
 
 class Part1Tests(unittest.TestCase):
 
@@ -74,6 +161,22 @@ class Part1Tests(unittest.TestCase):
         grid = Grid(utils.read_input())
         grid.run_until_layout_repeats()
         self.assertEqual(28778811, grid.biodiversity_rating())
+
+
+class Part2Tests(unittest.TestCase):
+
+    def test_with_example(self):
+        grid = RecursiveGrid(PART_2_EXAMPLE_INITIAL_STATE, 5)
+        for i in range(10):
+            grid.step()
+        self.assertEqual(PART_2_EXAMPLE_AFTER_10_MINS.strip(), grid.as_text())
+        self.assertEqual(99, grid.total_bugs())
+
+    def test_with_input(self):
+        grid = RecursiveGrid("\n".join(utils.read_input()), 150)
+        for i in range(200):
+            grid.step()
+        self.assertEqual(2097, grid.total_bugs())
 
 
 def read_example(text):
