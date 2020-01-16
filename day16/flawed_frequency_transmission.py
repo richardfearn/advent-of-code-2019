6e3dc35169c3a06cc3077c346d8111a4b642e694
Richard Fearn <richardfearn@gmail.com>
2020-01-16 12:37:52 +0000

    Day 16 - part 1 - improve performance
    Avoid recalculating the pattern used for each output digit

--- day16/flawed_frequency_transmission.py
+++ day16/flawed_frequency_transmission.py
@@ -3,19 +3,25 @@
 
 class FFT:
 
-    def __init__(self):
+    def __init__(self, signal):
         self.pattern = [0, 1, 0, -1]
+        self.values = [int(i) for i in signal]
+        self.patterns = [self.digit_pattern(out_digit) for out_digit in range(len(self.values))]
 
-    def calculate(self, inputs):
-        inputs = [int(i) for i in inputs]
-        outputs = [self.new_digit(inputs, i) for i in range(len(inputs))]
-        return "".join([str(o) for o in outputs])
+    def digit_pattern(self, out_digit):
+        return [self.pattern[self.pattern_index(in_digit, out_digit)] for in_digit in range(len(self.values))]
+
+    def pattern_index(self, in_digit, out_digit):
+        return ((in_digit + 1) // (out_digit + 1)) % len(self.pattern)
+
+    def calculate(self):
+        self.values = [self.new_digit(self.values, i) for i in range(len(self.values))]
 
     def new_digit(self, inputs, out_digit):
-        pattern = [self.pattern[self.pattern_index(in_digit, out_digit)] for in_digit in range(len(inputs))]
+        pattern = self.patterns[out_digit]
         parts = [inputs[in_digit] * pattern[in_digit] for in_digit in range(len(inputs))]
         digit = abs(sum(parts)) % 10
         return digit
 
-    def pattern_index(self, in_digit, out_digit):
-        return ((in_digit + 1) // (out_digit + 1)) % len(self.pattern)
+    def as_string(self):
+        return "".join([str(v) for v in self.values])
--- day16/tests.py
+++ day16/tests.py
@@ -9,49 +9,48 @@ class Part1Tests(unittest.TestCase):
 
     def test_example_1(self):
 
-        fft = FFT()
-
         signal = "12345678"
+        fft = FFT(signal)
 
-        signal = fft.calculate(signal)
-        self.assertEqual("48226158", signal)
+        fft.calculate()
+        self.assertEqual("48226158", fft.as_string())
 
-        signal = fft.calculate(signal)
-        self.assertEqual("34040438", signal)
+        fft.calculate()
+        self.assertEqual("34040438", fft.as_string())
 
-        signal = fft.calculate(signal)
-        self.assertEqual("03415518", signal)
+        fft.calculate()
+        self.assertEqual("03415518", fft.as_string())
 
-        signal = fft.calculate(signal)
-        self.assertEqual("01029498", signal)
+        fft.calculate()
+        self.assertEqual("01029498", fft.as_string())
 
     def test_larger_example_1(self):
-        fft = FFT()
         signal = "80871224585914546619083218645595"
+        fft = FFT(signal)
         for i in range(100):
-            signal = fft.calculate(signal)
-        self.assertEqual("24176176", signal[:8])
+            fft.calculate()
+        self.assertEqual("24176176", fft.as_string()[:8])
 
     def test_larger_example_2(self):
-        fft = FFT()
         signal = "19617804207202209144916044189917"
+        fft = FFT(signal)
         for i in range(100):
-            signal = fft.calculate(signal)
-        self.assertEqual("73745418", signal[:8])
+            fft.calculate()
+        self.assertEqual("73745418", fft.as_string()[:8])
 
     def test_larger_example_3(self):
-        fft = FFT()
         signal = "69317163492948606335995924319873"
+        fft = FFT(signal)
         for i in range(100):
-            signal = fft.calculate(signal)
-        self.assertEqual("52432133", signal[:8])
+            fft.calculate()
+        self.assertEqual("52432133", fft.as_string()[:8])
 
     def test_with_input(self):
         signal = read_input()
-        fft = FFT()
+        fft = FFT(signal)
         for i in range(100):
-            signal = fft.calculate(signal)
-        self.assertEqual("61149209", signal[:8])
+            fft.calculate()
+        self.assertEqual("61149209", fft.as_string()[:8])
 
 
 def read_input():
